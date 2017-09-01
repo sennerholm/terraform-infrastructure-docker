9085cac5c9d61c9f758b4e0486e8666926ed9ff0
Mikael Sennerholm <mikael@sennerholm.net>
2017-09-01 14:28:38 +0200

    Adding support for my-ssh-keys to handle ssh-keys

--- gocd-agent-gcloud/pre-docker-entrypoint.sh
+++ gocd-agent-gcloud/pre-docker-entrypoint.sh
@@ -6,5 +6,16 @@
 # Activate serviceaccount 
 gcloud auth activate-service-account --key-file=/var/run/secrets/cloud.google.com/service-account.json
 
+# Add ssh identity of github
+sudo -u go ssh -T -oStrictHostKeyChecking=no git@github.com || true
+
+# Copy (and chown /my-ssh-keys if exists)
+if [ -f /my-ssh-keys/id_dsa -o -f /my-ssh-keys/id_rsa ]; then
+	cp /my-ssh-keys/id_* ~go/.ssh
+	chown 600 ~go/.ssh/id_*
+	chown go:go ~go/.ssh/id_*
+fi
+
+
 # Starting the standard entrypoint
 exec /docker-entrypoint.sh "$@"
